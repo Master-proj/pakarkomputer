611134eda977c2dfdb1cfdc97256572a364616f2
Master-proj <master22.project@gmail.com>
2025-10-08 07:45:52 +0700

    update terbaru

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
       rel="stylesheet"/>
     <link rel="stylesheet" href="custom.css" />
     <title>Fix.CO</title>
+    <style>
+    </style>
   </head>
   <body>
     <nav class="navbar py-2 navbar-expand-lg navbar-light">
       <div class="container">
         <a class="navbar-brand" href="#">
-          <img src="gambar\logobaru.png" width="150" alt="logo" />
+          <h2>Pakar COM</h2>
         </a>
         <button
           class="navbar-toggler"
@@ -36,53 +38,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item align-self-center active">
-              <a class="nav-link" href="index.php"
-                >Home <span class="sr-only">(current)</span></a
-              >
+              <a class="nav-link" href="index.php"> Home <span class="sr-only">(current)</span>
+              </a>
             </li>
             <li class="nav-item align-self-center active">
               <a class="nav-link"  href="#alur">Alur Kerja <span class="sr-only">(current)</span></a>
             </li>
-            
-            <li class="nav-item ">
-              <!-- Button trigger modal -->
-              <button type="button" class="btn px-4 btn-secondary ml-5 logintombol" data-toggle="modal" data-target="#exampleModal">Log In</button>
-              </button>
-
-              <!-- Login -->
-              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Log In</h5>
-                      <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button> -->
-                    </div>
-                    <div class="modal-body">
-                      <form id="login-form" action="function.php?act=login" method="POST" >
-                        <div class="form-group">
-                          <div id="result"></div>
-                            <label for="nama" class="col-form-label">Username :</label>
-                              <input type="text" class="form-control" id="nama" name="nama" rows="3" placeholder="Username">
-                              <label for="pass" class="col-form-label">Password :</label>
-                              <input type="password" class="form-control" id="password" name="password" rows="3" placeholder="Password">
-                          </div>
-                        <div class="form-row">
-                    </div>
-                        <div class="modal-footer">
-                          <input type="submit" name="login_btn" id="login" class="btn btn-primary" value="Login">
-                        </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </li>
-            <li>
-              <a class="btn px-4 btn-primary ml-2" href="register.php" role="button"
-              >Register</a>
-            </li>
-            
           </ul>
         </div>
       </div>
@@ -99,7 +60,7 @@
             <a class="btn btn-primary" href="register.php" role="button">Ayo Mulai!</a>
           </div>
           <div class="col d-none d-sm-block">
-            <img width="500" src="gambar\herobaru.png" alt="hero" />
+            <img width="500" src="gambar\msl_khusus.png" alt="hero" />
           </div>
         </div>
       </div>
@@ -139,10 +100,62 @@
           </div>
         </div>
       </div>
-    </sect>
+    </section>
+    <!-- Footer -->
+<footer class="text-center text-lg-start text-dark" style="background-color: #f2f2f2; font-family: 'Poppins', sans-serif;">
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block">
+      <span>Terhubung dengan kami di media sosial:</span>
+    </div>
+
+    <div>
+      <a href="#" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="#" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+    </div>
+  </section>
+
+  <section>
+    <div class="container text-center text-md-start mt-4">
+      <div class="row mt-3">
+        <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-3">Sistem Pakar Servis Komputer</h6>
+          <p>
+            Aplikasi berbasis web yang membantu pengguna menganalisis permasalahan komputer secara cepat dan akurat melalui sistem pakar.
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-3">Navigasi</h6>
+          <p><a href="index.php" class="text-reset">Home</a></p>
+          <p><a href="#alur" class="text-reset">Alur Kerja</a></p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-3">Kontak</h6>
+          <p><i class="fas fa-home me-3"></i> Jakarta, Indonesia</p>
+          <p><i class="fas fa-envelope me-3"></i> info@serviskomputer.com</p>
+          <p><i class="fas fa-phone me-3"></i> +62 812 3456 7890</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <div class="text-center p-3" style="background-color: #e0e0e0;">
+    © 2025 Sistem Pakar Servis Komputer. All rights reserved.
+  </div>
+</footer>
 
   </body>
-
   <script
     src="https://code.jquery.com/jquery-3.4.1.js"
     integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
